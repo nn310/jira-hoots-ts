135a6da7c1eca0f6a2a6dff4f16e8d3fb08f06b2
zzn <3101216081@qq.com>
2021-10-14 14:51:39 +0800

    代表的意思，给前面接口设置后面泛型

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 
 function Login() {
+  const handleSubmit=(event:FormEvent<HTMLFormElement>)=>{
+    event.preventDefault()//阻止默认提交
+    const userName=(event.currentTarget.elements[0] as HTMLFormElement).value
+    const passWord=(event.currentTarget.elements[1] as HTMLFormElement).value
+                                                 // as 代表的意思，给前面接口设置后面泛型
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor={'username'}>用户名</label>
           <input type="text" id={'username'} />
